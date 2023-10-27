2e8c27aafe5dc47c3467afe6db49b01491ab8b96
psy_inf <sebastian@smesserschmidt.de>
2023-10-28 00:00:17 +0200

    adapting sandbox

--- apps/sandbox/main.cpp
+++ apps/sandbox/main.cpp
@@ -1,8 +1,9 @@
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
-#include <sdlpp.hpp>
-#include <stdio.h>
+#include <SDLApp.h>
+#include <iostream>
+
 #include <vector>
 
 struct Star
@@ -12,63 +13,42 @@ struct Star
     float z;
 };
 
-static void mainloop()
+static void mainloop(pg::SDLApp& sdlApp, std::vector<Star>& stars)
 {
-    sdl::Init   init(SDL_INIT_EVERYTHING);
-    sdl::Window w("Starfiled", 65, 126, 1280, 720, SDL_WINDOW_BORDERLESS);
-    SDL_SetHint(SDL_HINT_RENDER_VSYNC, "1");
-    sdl::Renderer     r(w.get(), -1, 0);
-    auto              done = false;
-    sdl::EventHandler e;
-    e.quit = [&done](const SDL_QuitEvent&) { done = true; };
-    std::vector<Star> stars;
-    for (int i = 0; i < 1000; ++i)
-        stars.push_back(Star{1.0f * (rand() % 1280), 1.0f * (rand() % 720), 0});
-    while (!done)
-    {
-        while (e.poll()) {}
-        r.setDrawColor(0x00, 0x00, 0x00, 0xff);
-        r.clear();
-        for (auto& star : stars)
-        {
-            if (star.z < 0xff) star.z += 1;
-            star.x = (star.x - 1280 / 2) * (1 + 0.0001 * star.z) + 1280 / 2;
-            star.y = (star.y - 720 / 2) * (1 + 0.0001 * star.z) + 720 / 2;
-            if (star.x < 0 || star.x > 1280 || star.y < 0 || star.y > 720)
-                star = Star{1.0f * (rand() % 1280), 1.0f * (rand() % 720), 0};
-            r.setDrawColor(star.z, star.z, star.z, 0xff);
-            r.drawPoint(star.x, star.y);
-        }
-        r.present();
-    }
+ 
 
-    /*
-    if (!game_is_still_running)
-    {
-        deinitialize_the_game();
-#ifdef __EMSCRIPTEN__
-        emscripten_cancel_main_loop(); // this should "kill" the app.
-#else
-        exit(0);
-#endif
-    }
 
-    check_for_new_input();
-    think_about_stuff();
-    draw_the_next_frame();
-    */
 }
 
 int main(int argc, char** argv)
+try
 {
-    // initialize_the_game();
-#ifdef __EMSCRIPTEN__
-    emscripten_set_main_loop(mainloop, 0, 1);
-#else
-    while (1)
+    pg::config::WindowConfig windowConfig{{65, 126}, {1280, 720}, "minimal demo"};
+    pg::SDLApp               sdlApp{windowConfig};
+
+
+      auto& renderer = sdlApp.getRenderer();
+    auto  done = false;
+    sdlApp.getEventHandler().quit = [&done](const SDL_QuitEvent&) {
+        std::cout << "bye!";
+        done = true;
+    };
+
+    while (!done)
     {
-        mainloop();
+        while (sdlApp.getEventHandler().poll()) {}
+        renderer->setDrawColor(0x00, 0x00, 0x00, 0xff);
+        renderer->clear();
+        renderer->setDrawColor(0xff, 0xff, 0xff, 0xff);
+        renderer->drawLine(0, 0, 1280, 720);
+        renderer->rectangleColor(100, 100, 200, 200, 0xff00ff00);
+        renderer->present();
     }
-#endif
+
     return 0;
+}
+catch (const std::exception& e)
+{
+    std::cerr << "Terminated: " << e.what() << std::endl;
+    return -1;
 }
