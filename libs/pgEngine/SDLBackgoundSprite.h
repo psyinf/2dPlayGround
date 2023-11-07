bed19f1d52a7dc106ffd4e7e724ca1e4d25b3c77
S. Messerschmidt <sebastian@smesserschmidt.de>
2023-11-07 21:45:41 +0100

    Adapted to float coordinates in transform (#5)

--- libs/pgEngine/SDLBackgoundSprite.h
+++ libs/pgEngine/SDLBackgoundSprite.h
@@ -16,27 +16,27 @@ public:
     {
         auto& texture_rect = getTextureRect();
         getTexture().query(nullptr, nullptr, &texture_rect.w, &texture_rect.h);
-        auto t = trans;
-        t.pos[0] = std::fmod(t.pos[0], texture_rect.w);
-        if (t.pos[0] > 0) { t.pos[0] -= texture_rect.w; }
-        t.pos[1] = std::fmod(t.pos[1], texture_rect.h);
-        if (t.pos[1] > 0) { t.pos[1] -= texture_rect.h; }
+        auto pos = iVec2{static_cast<int>(trans.pos[0]), static_cast<int>(trans.pos[1])};
+        pos[0] %= texture_rect.w;
+        if (pos[0] > 0) { pos[0] -= texture_rect.w; }
+        pos[1] %= texture_rect.h;
+        if (pos[1] > 0) { pos[1] -= texture_rect.h; }
         SDL_Rect src_1_rect{0, 0, texture_rect.w, texture_rect.h};
-        SDL_Rect dst_1_rect{t.pos[0], t.pos[1], texture_rect.w, texture_rect.h};
+        SDL_Rect dst_1_rect{pos[0], pos[1], texture_rect.w, texture_rect.h};
 
-        SDL_Rect src_2_rect{0, 0, -t.pos[0], texture_rect.h};
-        SDL_Rect dst_2_rect{texture_rect.w + t.pos[0], +t.pos[1], -t.pos[0], texture_rect.h};
+        SDL_Rect src_2_rect{0, 0, -pos[0], texture_rect.h};
+        SDL_Rect dst_2_rect{texture_rect.w + pos[0], +pos[1], -pos[0], texture_rect.h};
 
-        SDL_Rect src_3_rect{0, 0, texture_rect.w, -t.pos[1]};
-        SDL_Rect dst_3_rect{t.pos[0], texture_rect.h + t.pos[1], texture_rect.w, -t.pos[1]};
+        SDL_Rect src_3_rect{0, 0, texture_rect.w, -pos[1]};
+        SDL_Rect dst_3_rect{pos[0], texture_rect.h + pos[1], texture_rect.w, -pos[1]};
 
-        SDL_Rect src_4_rect{0, 0, -t.pos[0], -t.pos[1]};
-        SDL_Rect dst_4_rect{texture_rect.w + t.pos[0], texture_rect.h + t.pos[1], -t.pos[0], -t.pos[1]};
+        SDL_Rect src_4_rect{0, 0, -pos[0], -pos[1]};
+        SDL_Rect dst_4_rect{texture_rect.w + pos[0], texture_rect.h + pos[1], -pos[0], -pos[1]};
 
-        r.copyEx(getTexture().get(), &src_1_rect, &dst_1_rect, t.rotation_deg, nullptr, SDL_FLIP_NONE);
-        r.copyEx(getTexture().get(), &src_2_rect, &dst_2_rect, t.rotation_deg, nullptr, SDL_FLIP_NONE);
-        r.copyEx(getTexture().get(), &src_3_rect, &dst_3_rect, t.rotation_deg, nullptr, SDL_FLIP_NONE);
-        r.copyEx(getTexture().get(), &src_4_rect, &dst_4_rect, t.rotation_deg, nullptr, SDL_FLIP_NONE);
+        r.copyEx(getTexture().get(), &src_1_rect, &dst_1_rect, trans.rotation_deg, nullptr, SDL_FLIP_NONE);
+        r.copyEx(getTexture().get(), &src_2_rect, &dst_2_rect, trans.rotation_deg, nullptr, SDL_FLIP_NONE);
+        r.copyEx(getTexture().get(), &src_3_rect, &dst_3_rect, trans.rotation_deg, nullptr, SDL_FLIP_NONE);
+        r.copyEx(getTexture().get(), &src_4_rect, &dst_4_rect, trans.rotation_deg, nullptr, SDL_FLIP_NONE);
     }
 
 private:
