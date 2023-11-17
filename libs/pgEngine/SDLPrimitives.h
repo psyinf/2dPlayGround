4d08960254fcb7d93ea29003d3424bed151b3c5b
psy_inf <sebastian@smesserschmidt.de>
2023-11-18 00:53:57 +0100

    add linestrip, points and scopedScale reset

--- libs/pgEngine/SDLPrimitives.h
+++ libs/pgEngine/SDLPrimitives.h
@@ -4,7 +4,22 @@
 #include "SDLBounds.h"
 namespace pg {
 
+class ScopedScale
+{
+public:
+    ScopedScale(sdl::Renderer& r, const fVec2& setScale)
+      : renderer(r)
+    {
+        renderer.getScale(&scale[0], &scale[1]);
+        renderer.setScale(setScale[0], setScale[1]);
+    }
 
+    ~ScopedScale() { renderer.setScale(scale[0], scale[1]); }
+
+private:
+    sdl::Renderer& renderer;
+    fVec2          scale;
+};
 
 class Primitive
 {
@@ -36,6 +51,54 @@ public:
 
 protected:
     iVec2 pos;
+};
+
+class LineStrip : public pg::Primitive
+{
+public:
+    LineStrip(std::vector<iVec2>&& points)
+      : points(points)
+    {
+    }
+
+    void draw(sdl::Renderer& r, const Transform& t) override
+    {
+        r.setDrawColor(255, 255, 255, 255);
+        // draw the polygon
+        for (const auto& p : points)
+        {
+            r.drawLines(std::bit_cast<SDL_Point*>(points.data()), points.size());
+        }
+    }
+
+    void draw(sdl::Renderer& r) { draw(r, {}); }
+
+private:
+    std::vector<iVec2> points;
+};
+
+class Points : public pg::Primitive
+{
+public:
+    Points(std::vector<iVec2>&& points)
+      : points(points)
+    {
+    }
+
+    void draw(sdl::Renderer& r, const Transform& t) override
+    {
+        r.setDrawColor(255, 255, 255, 255);
+        // draw the polygon
+        for (const auto& p : points)
+        {
+            r.drawPoints(std::bit_cast<SDL_Point*>(points.data()), points.size());
+        }
+    }
+
+    void draw(sdl::Renderer& r) { draw(r, {}); }
+
+private:
+    std::vector<iVec2> points;
 };
 
 } // namespace pg
