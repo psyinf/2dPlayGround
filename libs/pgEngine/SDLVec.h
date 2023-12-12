bb80f58ebb4134b03e23f6cd0ecad822e0924a58
psy_inf <sebastian@smesserschmidt.de>
2023-12-13 00:48:35 +0100

    clean-up and added generic method for vector operations

--- libs/pgEngine/SDLVec.h
+++ libs/pgEngine/SDLVec.h
@@ -27,10 +27,20 @@ using Color = Vec4<uint8_t>;
 
 } // namespace pg
 
+template <typename T, size_t SIZE, typename Functor>
+auto elementWise(const Functor& f, const pg::Vec<T, SIZE>& rhs)
+{
+    pg::Vec<T, SIZE> res;
+    for (auto idx : std::views::iota(size_t{}, res.size()))
+    {
+        res[idx] = f(rhs[idx]);
+    }
+    return res;
+}
+
 template <typename T, size_t SIZE>
 constexpr pg::Vec<T, SIZE> operator+(const pg::Vec<T, SIZE>& lhs, const pg::Vec<T, SIZE>& rhs)
 {
-    // for now
     pg::Vec<T, SIZE> res;
     for (auto idx : std::views::iota(size_t{}, lhs.size()))
     {
@@ -42,11 +52,19 @@ constexpr pg::Vec<T, SIZE> operator+(const pg::Vec<T, SIZE>& lhs, const pg::Vec<
 template <typename T, size_t SIZE>
 void operator+=(pg::Vec<T, SIZE>& lhs, const pg::Vec<T, SIZE>& rhs)
 {
-    // for now
-    pg::Vec<T, SIZE> res;
     for (auto idx : std::views::iota(size_t{}, lhs.size()))
     {
         lhs[idx] += rhs[idx];
+    }
+    return lhs;
+}
+
+template <typename T, size_t SIZE>
+constexpr auto operator*=(pg::Vec<T, SIZE>& lhs, const pg::Vec<T, SIZE>& rhs)
+{
+    for (auto idx : std::views::iota(size_t{}, lhs.size()))
+    {
+        lhs[idx] *= rhs[idx];
     }
     return lhs;
 }
