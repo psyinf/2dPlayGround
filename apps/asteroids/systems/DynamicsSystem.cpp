e50fc879af94ac1717ba2980e11041fe55dbb80d
psy_inf <sebastian@smesserschmidt.de>
2023-12-13 09:37:30 +0100

    DynamicsSystem to move all position updates to separate system

---
+++ apps/asteroids/systems/DynamicsSystem.cpp
@@ -0,0 +1,20 @@
+#include "DynamicsSystem.hpp"
+#include "core/Game.h"
+#include "Player.h"
+#include "entities/Entities.h"
+
+void game::DynamicsSystem::setup() {}
+
+void game::DynamicsSystem::handle(const FrameStamp& frameStamp)
+{
+    auto& registry = game.getRegistry();
+
+    auto view = game.getRegistry().view<pg::Transform, game::Dynamics>();
+    for (auto& entity : view)
+    {
+        auto&& [transform, dynamics] = view.get<pg::Transform, game::Dynamics>(entity);
+        //cut of remainder to avoid jittering
+        transform.pos += elementWise(std::truncl, dynamics.velocity * frameStamp.getFrameDuration_sec());
+        dynamics.velocity = dynamics.velocity * dynamics.dampening;
+    }
+}
