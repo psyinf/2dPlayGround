0defc99a44f9fb3b649a1bdb6bc65856aea35798
psy_inf <sebastian@smesserschmidt.de>
2023-12-13 21:15:06 +0100

    try __cpp macro

--- libs/pgEngine/SDLErrorTrace.h
+++ libs/pgEngine/SDLErrorTrace.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-#ifdef USE_STACKTRACES
+#ifdef __cpp_lib_stacktrace
 #include <stacktrace>
 #endif
 
@@ -8,7 +8,7 @@ namespace errorTrace {
 
 inline void printErrorTrace()
 {
-#ifdef USE_STACKTRACES
+#ifdef __cpp_lib_stacktrace
     auto&& trace = std::stacktrace::current();
     std::cerr << "Stacktrace:\n" << trace << std::endl;
 #endif
