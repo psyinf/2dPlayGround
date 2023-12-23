2e6064c36efbe98160b8be2e892a840798c7a9e1
S. Messerschmidt <sebastian@smesserschmidt.de>
2023-12-23 11:37:43 +0100

    Text rendering (#16)
    * added sdl_ttf to conan dependencies
    
    * added textdemo application stub
    
    * Conformity fix
    
    * test font
    
    * sprite factory for text
    
    * RAII Lifetime wrapper API
    
    TTF library init/deinit RAII-style
    
    * Text demo
    
    * round up
    
    * Use conanfile in conjunction with conan cmake integration
    
    * fix dependency missing
    
    * fix deleter scope
    
    * corrected include for SDL TTF
    
    indirection to allow for header inclusion on private scope
    
    * SDL TTF as private dependency
    
    renamed Factories.h and split off implementation to cpp

---
+++ apps/textdemo/main.cpp
@@ -0,0 +1,32 @@
+
+#include <Factories.hpp>
+#include <SDLApp.h>
+#include <SDLErrorTrace.h>
+#include <SDLPrimitives.h>
+
+
+void textdemo_main()
+{
+    pg::SDLApp app{pg::config::WindowConfig{.screen{}, .offset{200, 200}, .size{1024, 1024}}};
+
+    // make a text sprite
+    auto text =
+        pg::SpriteFactory::makeTextSprite(app.getRenderer(), "Hello World", "../data/fonts/Roboto-Regular.ttf", 24);
+    // draw it
+    float rot = 0.0;
+    // TODO: test approach where we render the text as white and then use blending to color it
+    auto render = [&](auto& app) {
+        text.draw(app.getRenderer(),
+                  {.pos{100, 100}, .rotation_deg{rot += 0.1}, .scale{static_cast<float>(std::sin(rot * 0.1)), 0.5}});
+        text.draw(app.getRenderer(), {.pos{512, 512}});
+    };
+
+    auto done = false;
+    app.loop(done, render);
+}
+
+int main(int argc, char** argv)
+{
+    textdemo_main();
+    return 0;
+}
