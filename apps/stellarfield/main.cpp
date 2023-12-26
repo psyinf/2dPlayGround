e208e22e9f1eddc57b71e70dfd419fcd2fed3c97
psy_inf <sebastian@smesserschmidt.de>
2023-12-27 00:27:01 +0100

    minimal test with random sprites

---
+++ apps/stellarfield/main.cpp
@@ -0,0 +1,65 @@
+#include <Factories.hpp>
+#include <SDLApp.h>
+#include <SDLErrorTrace.h>
+#include <SDLPrimitives.h>
+#include <States.hpp>
+
+#include <random>
+
+pg::fVec2 makeRandom(const pg::fVec2& minMaxX, const pg::fVec2& minMaxY)
+{
+    // use random distribution to generate random numbers between min and max
+    static std::random_device                    rd;
+    static std::mt19937                          gen(rd());
+    static std::uniform_real_distribution<float> disX(minMaxX[0], minMaxX[1]);
+    static std::uniform_real_distribution<float> disY(minMaxY[0], minMaxY[1]);
+
+    return {disX(gen), disY(gen)};
+}
+
+void createStars(std::vector<pg::fVec2>& positions, size_t count, const pg::fVec2& pos, const pg::fVec2& width)
+{
+    positions.clear();
+    positions.reserve(count);
+    for (size_t i = 0; i < count; ++i)
+    {
+        positions.emplace_back(makeRandom({pos[0], pos[0] + width[0]}, {pos[1], pos[1] + width[1]}));
+    }
+}
+
+void textdemo_main()
+{
+    pg::SDLApp app{pg::config::WindowConfig{.screen{}, .offset{200, 200}, .size{1024, 1024}}};
+
+    auto font = pg::SDLFont("../data/fonts/Roboto-Regular.ttf", 24);
+    // make a text sprite
+    auto dot = pg::SpriteFactory::makeSprite(app.getRenderer(), "../data/circle_05.png");
+    auto line = pg::Line{{0, 0}, {100, 100}};
+
+    auto rendererStates = pg::States{};
+    rendererStates.push(pg::TextureColorState{pg::Color{255, 255, 0, 255}});
+    rendererStates.push(pg::TextureBlendModeState{SDL_BLENDMODE_ADD});
+    //
+    std::vector<pg::fVec2> positions;
+    auto                   bounds = app.getDisplayBounds(0);
+    createStars(positions,
+                10000,
+                vec_cast<float>(pg::iVec2{bounds.x, bounds.y}),
+                vec_cast<float>(pg::iVec2{bounds.w, bounds.h}));
+
+    auto render = [&](auto& app) {
+        for (const auto& pos : positions)
+        {
+            dot.draw(app.getRenderer(), {.pos{pos}, .scale{0.05, 0.05}}, rendererStates);
+        }
+    };
+
+    auto done = false;
+    app.loop(done, render);
+}
+
+int main(int argc, char** argv)
+{
+    textdemo_main();
+    return 0;
+}
