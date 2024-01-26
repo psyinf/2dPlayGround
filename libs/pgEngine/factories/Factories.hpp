7129a780d49015091de335d8b03023663c2613e6
psy_inf <sebastian@smesserschmidt.de>
2024-01-26 21:54:24 +0100

    FramedSprite implementation, not yet ready

--- libs/pgEngine/factories/Factories.hpp
+++ libs/pgEngine/factories/Factories.hpp
@@ -1,10 +1,11 @@
 #pragma once
-#include <pgEngine/font/Font.hpp>
-#include <pgEngine/primitives/Sprite.hpp>
-
-class SDLFont;
+#include "SDLFont.hpp"
+#include "SDLSprite.h"
+#include <sdlpp.hpp>
 
 namespace pg {
+// TODO: get from resource manager
+// TODO: don't return sprites but textures
 
 class SpriteFactory
 {
@@ -12,6 +13,18 @@ public:
     static Sprite makeSprite(sdl::Renderer& renderer, std::string_view resource_name);
 
     static Sprite makeTextSprite(sdl::Renderer& renderer, SDLFont& font, std::string_view text);
+    /**
+     * @brief
+     * @param renderer
+     * @param width num of horizontal frames
+     * @param height num of vertical frames
+     * @param resource_name
+     * @return
+     */
+    static FramedSprite makeFramedSprite(sdl::Renderer&   renderer,
+                                         uint16_t         width,
+                                         uint16_t         height,
+                                         std::string_view resource_name);
 };
 
 } // namespace pg
