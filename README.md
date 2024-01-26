6ad51eda86ec81d17982f9b55c4ff6450247e811
psy_inf <sebastian@smesserschmidt.de>
2024-01-26 22:04:36 +0100

    adding documentation on how to build and setup

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # 2dPlayGround
 
 A small playground to create a mini-game based on SDL, entt using modern C++, cmake, CPM and conan
+
+## Prerequisites
+The project uses CMake Package Manager and Conan for dependencies. In order to setup your environment check the /doc/setup.sh script
---
+++ doc/setup_env.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+if [ -z "$1" ]
+  then
+    echo "No argument supplied, please supply a non-root user name as first argument "
+fi
+USER="$1"
+
+apt -qq update
+apt -q install -y cmake ninja-build g++ clang pkgconf
+apt -q install -y python3-pip
+
+sudo -Hiu $USER env pip3 install --user conan==1.60.2
+sudo -Hiu $USER env conan profile new default --detect
+sudo -Hiu $USER env conan profile update settings.compiler.libcxx=libstdc++11 default
+sudo -Hiu $USER env conan profile update conf.tools.system.package_manager:mode=install default
+sudo -Hiu $USER env conan profile update conf.tools.system.package_manager:sudo=True default
