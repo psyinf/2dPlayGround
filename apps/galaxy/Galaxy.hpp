cdc125f4681f3ec6f20e7cf25bb7be7e63c119d7
psy_inf <sebastian@smesserschmidt.de>
2024-02-06 22:56:30 +0100

    proper zoom and pan

--- apps/galaxy/Galaxy.hpp
+++ apps/galaxy/Galaxy.hpp
@@ -2,7 +2,9 @@
 #include <pgGame/core/Game.hpp>
 #include <pgGame/core/RegistryHelper.hpp>
 #include <pgGame/entities/Drawable.hpp>
+
 #include <pgEngine/primitives/Sprite.hpp>
+#include <pgEngine/math/VecUtils.hpp>
 #include <systems/RenderSystem.hpp>
 #include <systems/UpdateSystem.hpp>
 
@@ -26,15 +28,19 @@ public:
         systems.emplace_back(std::make_unique<galaxy::RenderSystem>(*game));
         systems.emplace_back(std::make_unique<galaxy::UpdateSystem>(*game));
 
-        game->getKeyStateMap().registerMouseRelativeDraggedCallback(
-            [&scene](auto pos, auto state) { scene.getGlobalTransform().pos += vec_cast<float>(pos); });
+        game->getKeyStateMap().registerMouseRelativeDraggedCallback([&scene](auto pos, auto state) {
+            scene.getGlobalTransform().pos += vec_cast<float>(pos) * (1.0f / scene.getGlobalTransform().scale);
+        });
 
         game->getKeyStateMap().registerMouseWheelCallback([&scene](auto pos) {
             scene.getGlobalTransform().scale *= std::powf(1.1f, pos[1]);
             scene.getGlobalTransform().scale[0] = std::clamp(scene.getGlobalTransform().scale[0], 0.5f, 10.0f);
             scene.getGlobalTransform().scale[1] = std::clamp(scene.getGlobalTransform().scale[1], 0.5f, 10.0f);
         });
+
+        game->getKeyStateMap().registerKeyCallback(SDLK_SPACE, [&scene](auto) { scene.getGlobalTransform() = {}; });
         setupStarSystems();
+        // setupRegularGrid();
         game->switchScene("start");
     }
 
@@ -52,6 +58,29 @@ private:
                 {.pos{pg::getPointInCircle(375)}, .scale{0.025, 0.025}},
                 pg::game::Drawable{dot_sprite},
                 galaxy::StarSystemState{});
+        }
+    }
+
+    void setupRegularGrid()
+    {
+        // centered around 0,0
+        auto windowRect = game->getSingleton<pg::game::WindowDetails>().windowRect;
+        auto dot_sprite = game->getTypedResourceCache<pg::Sprite>().load("../data/circle_05.png");
+        auto midpoint = pg::dimsFromRect<float>(windowRect) * 0.5f;
+
+        for (auto x : std::ranges::iota_view{0, windowRect.w / 10})
+        {
+            for (auto y : std::ranges::iota_view{0, windowRect.h / 10})
+            {
+                pg::game::makeEntity<pg::Transform2D, pg::game::Drawable, galaxy::StarSystemState>(
+                    game->getRegistry(),
+                    {.pos{midpoint - 10.0f * pg::fVec2{static_cast<float>(x), static_cast<float>(y)}},
+                     .scale{0.025, 0.025}},
+                    pg::game::Drawable{dot_sprite},
+                    galaxy::StarSystemState{(x == windowRect.w / 20 && y == windowRect.h / 20)
+                                                ? ColonizationStatus::Colonized
+                                                : ColonizationStatus::Unexplored});
+            }
         }
     }
 
--- apps/galaxy/systems/RenderSystem.cpp
+++ apps/galaxy/systems/RenderSystem.cpp
@@ -3,6 +3,7 @@
 #include <pgGame/entities/Drawable.hpp>
 #include <pgGame/core/Game.hpp>
 #include <pgGame/entities/WindowDetails.hpp>
+#include <pgEngine/math/VecUtils.hpp>
 
 void galaxy::RenderSystem::setup() {}
 
@@ -14,16 +15,30 @@ void galaxy::RenderSystem::handle(const pg::game::FrameStamp& frameStamp)
     // rendererStates.push(pg::TextureColorState{pg::Color{255, 255, 0, 255}});
     rendererStates.push(pg::TextureBlendModeState{SDL_BLENDMODE_ADD});
     rendererStates.apply(renderer);
-
+    auto windowRect = game.getSingleton<pg::game::WindowDetails>().windowRect;
     auto globalTransform = game.getSingleton<pg::Transform2D>(pg::game::Scene::GlobalTransformName);
 
-    for (auto view = game.getRegistry().view<pg::game::Drawable, pg::Transform2D>(); auto& entity : view)
+    for (auto  view = game.getRegistry().view<pg::game::Drawable, pg::Transform2D, galaxy::StarSystemState>();
+         auto& entity : view)
     {
-        auto&& [drawable, transform] = view.get<pg::game::Drawable, pg::Transform2D>(entity);
+        auto&& [drawable, transform, systemState] =
+            view.get<pg::game::Drawable, pg::Transform2D, galaxy::StarSystemState>(entity);
         auto new_transform = transform;
-        new_transform.scale *= globalTransform.scale;
-        new_transform.pos = (transform.pos) * globalTransform.scale;
-        drawable.prim->draw(renderer, new_transform + globalTransform, rendererStates);
+        new_transform.pos -= pg::dimsFromRect<float>(windowRect) * 0.5f;
+        new_transform.pos = (globalTransform.pos + transform.pos) * globalTransform.scale;
+        new_transform.pos += pg::dimsFromRect<float>(windowRect) * 0.5f;
+        switch (systemState.colonizationStatus)
+        {
+        case galaxy::ColonizationStatus::Colonized:
+            rendererStates.push(pg::TextureColorState{pg::Color{255, 255, 0, 255}});
+            drawable.prim->draw(renderer, new_transform, rendererStates);
+            rendererStates.pop<pg::TextureColorState>();
+            break;
+
+        default:
+            drawable.prim->draw(renderer, new_transform, rendererStates);
+            break;
+        }
     }
     rendererStates.restore(renderer);
     renderer.present();
