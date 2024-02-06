e04cf9c67655d4fc836080c002d4956ba382f750
psy_inf <sebastian@smesserschmidt.de>
2024-02-06 22:54:21 +0100

    defaulting back to zero transform

--- libs/pgGame/core/Scene.cpp
+++ libs/pgGame/core/Scene.cpp
@@ -4,10 +4,6 @@
 
 void pg::game::Scene::setup(Game& game)
 {
-    const auto& window_details = game.getSingleton<pg::game::WindowDetails>();
-    const auto  midpoint = pg::fVec2{window_details.windowRect.w / 2.0f, window_details.windowRect.h / 2.0f};
-    globalTransform.pos = midpoint;
-
     game.addSingleton_as<pg::Transform2D&>(GlobalTransformName, globalTransform);
     std::ranges::for_each(systems, [](auto& system) { system->setup(); });
 }
