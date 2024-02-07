f09b77007cdc4ae49d56136cc886ea518c37d3e6
psy_inf <sebastian@smesserschmidt.de>
2024-02-07 22:58:45 +0100

    Box class and some tests

---
+++ libs/pgEngine/math/Box.hpp
@@ -0,0 +1,59 @@
+#pragma once
+#include <array>
+#include <pgEngine/math/Vec.hpp>
+
+// based on https://github.com/pvigier/Quadtree/blob/master/include/Box.h
+namespace pg {
+template <typename T>
+class Box
+{
+public:
+    Vec2<T> pos;
+    Vec2<T> dim;
+
+    constexpr Box(const Vec2<T>& offset, const Vec2<T>& dims) noexcept
+      : pos{offset}
+      , dim{dims}
+    {
+    }
+
+    constexpr decltype(auto) getMin() const noexcept { return pos; }
+
+    constexpr decltype(auto) getMax() const noexcept { return pos + dim; }
+
+    constexpr decltype(auto) left() const noexcept { return pos[0]; }
+
+    constexpr decltype(auto) right() const noexcept { return pos[0] + dim[0]; }
+
+    constexpr decltype(auto) top() const noexcept { return pos[1]; }
+
+    constexpr decltype(auto) bottom() const noexcept { return pos[1] + dim[1]; }
+
+    // TODO: check with integer types
+    constexpr decltype(auto) center() const noexcept { return (pos + dim) * static_cast<T>(0.5); }
+
+    constexpr decltype(auto) size() const noexcept { return dim; }
+
+    constexpr bool contains(const Box<T>& box) const noexcept
+    {
+        // Check if the other box is within the current box
+        return this->left() <= box.left() && this->right() >= box.right() && this->top() <= box.top() &&
+               this->bottom() >= box.bottom();
+    }
+
+    constexpr bool intersects(const Box<T>& box) const noexcept
+    {
+        // Check if box is to the right
+        if (this->left() >= box.right()) return false;
+        // Check if box is to the left
+        if (this->right() <= box.left()) return false;
+        // Check if box is above
+        if (this->bottom() <= box.top()) return false;
+        // Check if box is below
+        if (this->top() >= box.bottom()) return false;
+        return true;
+    }
+};
+
+using fBox = Box<float>;
+} // namespace pg
---
+++ tests/Bounds/BoxTests.cpp
@@ -0,0 +1,49 @@
+#include <pgEngine/math/Box.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Construction", "[BoxTests]")
+{
+    pg::fBox b1{{0.0, 1.0}, {2.0, 3.0}};
+
+    REQUIRE(b1.pos == pg::fVec2{0.0, 1.0});
+    REQUIRE(b1.dim == pg::fVec2{2.0, 3.0});
+}
+
+TEST_CASE("Contains", "[BoxTests]")
+{
+    pg::fBox box1({0, 0}, {10, 10});
+    pg::fBox box2({5, 5}, {2, 2});
+    pg::fBox box3({-5, -5}, {2, 2});
+    pg::fBox box4({5, 5}, {10, 10});
+
+    REQUIRE(box1.contains(box2));
+    REQUIRE_FALSE(box1.contains(box3));
+    REQUIRE_FALSE(box1.contains(box4));
+}
+
+TEST_CASE("Intersects", "[BoxTests]")
+{
+    pg::fBox box1({0, 0}, {10, 10});
+    pg::fBox box2({5, 5}, {2, 2});
+    pg::fBox box3({-5, -5}, {2, 2});
+    pg::fBox box4({5, 5}, {10, 10});
+    pg::fBox box5({10, 10}, {2, 2});
+    pg::fBox box6({9.99, 0}, {2, 2});
+    REQUIRE(box1.intersects(box2));
+    REQUIRE_FALSE(box1.intersects(box3));
+    REQUIRE(box1.intersects(box4));
+    REQUIRE_FALSE(box1.intersects(box5));
+    REQUIRE(box1.intersects(box6));
+}
+
+TEST_CASE("Getters", "[BoxTests]")
+{
+    pg::fBox box({0, 0}, {10, 10});
+
+    REQUIRE(box.left() == 0.0f);
+    REQUIRE(box.right() == 10.0f);
+    REQUIRE(box.top() == 0.0f);
+    REQUIRE(box.bottom() == 10.0f);
+    REQUIRE(box.center() == pg::fVec2({5.0f, 5.0f}));
+    REQUIRE(box.size() == pg::fVec2({10.0f, 10.0f}));
+}
