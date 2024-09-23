0923865fa018774d3f5cd41580a944a7e122e553
psy_inf <sebastian@smesserschmidt.de>
2024-09-23 23:42:31 +0200

    structured concept

---
+++ apps/productionSandbox/Types.hpp
@@ -0,0 +1,60 @@
+#pragma once
+#include <string>
+#include <vector>
+
+// entities representing resources
+using Amount = float;
+using Fraction = float;
+using Name = std::string;
+
+// todo: traits that mark resources, intermediate products, and finished products etc
+
+// Asset is a resource such as a raw material, a finished product, or energy
+struct Asset
+{
+    // TODO: use a monostate to enforce that the name is unique
+    // TODO: disallow empty names
+    Asset(const Name& name)
+      : name{name}
+    {
+    }
+
+    virtual ~Asset() = default;
+    const Name name;
+};
+
+struct Requirement
+{
+    Requirement(const Asset& asset, Amount amount = 1)
+      : name{asset.name}
+      , amount{amount}
+    {
+    }
+
+    Name   name;
+    Amount amount; // relative amount. e.g. if two requirements are needed, one with 1 and the other with 2 the ratio is
+                   // 1:2 to produce a product
+};
+
+struct Product : public Asset
+{
+    template <typename... Args>
+    Product(const Name& name, Args... args)
+      : Asset{name}
+      , _requirements{args...}
+      , _fraction{1.0f}
+    {
+    }
+
+    template <typename... Args>
+    Product(const Name& name, float minimalFraction, Args... args)
+      : Asset{name}
+      , _requirements{args...}
+      , _fraction{minimalFraction}
+    {
+    }
+
+    const std::vector<Requirement> _requirements;     // requirements to produce this product
+    const Fraction                 _fraction{1.0f};   // minimal fraction of the product that can be produced.
+    const float                    _complexity{1.0f}; // complexity of the product
+};
--- apps/productionSandbox/main.cpp
+++ apps/productionSandbox/main.cpp
@@ -1,105 +1,10 @@
+#include "Types.hpp"
+#include "Factory.hpp"
+#include <deque>
 #include <optional>
-#include <string>
-#include <unordered_map>
-#include <vector>
 
-#include <future>
-#include <expected>
-// entities representing resources
-using Amount = float;
-using Name = std::string;
-
-// todo: traits that mark resources, intermediate products, and finished products etc
-
-// Asset is a resource such as a raw material, a finished product, or energy
-struct Asset
+struct Factory2
 {
-    // TODO: use a monostate to enforce that the name is unique
-    // TODO: disallow empty names
-    Asset(const Name& name)
-      : name{name}
-    {
-    }
-
-    virtual ~Asset() = default;
-    const Name name;
-};
-
-struct Requirement
-{
-    Requirement(const Asset& asset, Amount amount = 1)
-      : name{asset.name}
-      , amount{amount}
-    {
-    }
-
-    Name   name;
-    Amount amount; // relative amount. e.g. if two requirements are needed, one with 1 and the other with 2 the ratio is
-                   // 1:2 to produce a product
-};
-
-struct Product : public Asset
-{
-    template <typename... Args>
-    Product(const Name& name, Args... args)
-      : Asset{name}
-      , _requirements{args...}
-      , _fraction{1.0f}
-    {
-    }
-
-    template <typename... Args>
-    Product(const Name& name, float minimalFraction, Args... args)
-      : Asset{name}
-      , _requirements{args...}
-      , _fraction{minimalFraction}
-    {
-    }
-
-    const std::vector<Requirement> _requirements;   // requirements to produce this product
-    const float                    _fraction{1.0f}; // minimal fraction of the product that can be produced.
-};
-
-struct Storage
-{
-    std::unordered_map<Name, Amount> resources;
-};
-
-struct Factory
-{
-    using Task = std::function<void()>;
-    using TaskList = std::vector<Task>;
-    Factory() = default;
-
-    std::optional<TaskList> plan(Storage& input_storage, Storage& output_storage, const Product& product)
-    {
-        // list of operations to reduce the amount of resources in storage
-        TaskList operations;
-        // check and remove resources from storage
-        for (const auto& requirement : product._requirements)
-        {
-            if (input_storage.resources[requirement.name] < requirement.amount) { return std::nullopt; }
-            operations.emplace_back(
-                [&input_storage, &requirement]() { input_storage.resources[requirement.name] -= requirement.amount; });
-        }
-        operations.emplace_back(
-            [&output_storage, &product]() mutable { output_storage.resources[product.name] += product._fraction; });
-        // all requirements are met, remove resources
-        return operations;
-    }
-
-    std::future<bool> make(Storage& input_storage, Storage& output_storage, const Product& product)
-    {
-        auto operations = plan(input_storage, output_storage, product);
-        if (!operations) { return std::future<bool>(); }
-        return std::async(std::launch::async, [operations = std::move(*operations)]() {
-            for (const auto& operation : operations)
-            {
-                operation();
-            }
-            return true;
-        });
-    }
 };
 
 using Resource = Asset;
@@ -111,6 +16,55 @@ const Resource Rare_Ores{"Rare Ores"};
 const Resource Energy{"Energy"};
 
 Material metals{"Metals", Requirement{Ores, 1}};
+
+// possible production = net_line_capactity / complexity
+class ProductionLine
+{
+    using ProductAmount = std::pair<Product, Amount>;
+    void tick(){};
+
+    void enqueue(const Product& product, Amount amount) { queue.emplace_back(product, amount); }
+
+    void process()
+    {
+        if (!current_product)
+        {
+            if (queue.empty()) { return; }
+            current_product.emplace(queue.front());
+            queue.pop_front();
+        }
+        // check the remaining amount of product to produce
+        auto&& [product, amount] = current_product.value();
+        // check how many fractions of the product could be produced
+        auto max_stored = input_storage.get_max_amount(product._requirements);
+        // round up to the fractional amount
+        auto max_produced = std::floor(max_stored / product._fraction);
+        // adapt to complexity and capacity
+        auto max_produced_by_line = std::min(max_produced, capacity / product._complexity);
+        // minimum from remaining amount and max_produced_by_line
+        auto to_produce = std::min(amount, max_produced_by_line);
+        // commit resources
+        for (const auto& requirement : product._requirements)
+        {
+            input_storage.take(requirement.name, requirement.amount * to_produce);
+        }
+        // remove produced fraction from the current product
+        amount -= to_produce;
+        // if the product is fully produced, remove it from the current product and put it in the output storage
+
+        if (amount == 0)
+        {
+            output_storage.put(product.name, to_produce);
+            current_product.reset();
+        }
+    }
+
+    float                        capacity;
+    std::optional<ProductAmount> current_product;
+    std::deque<ProductAmount>    queue;
+    Storage&                     input_storage;
+    Storage&                     output_storage;
+};
 
 int main()
 {
@@ -125,5 +79,6 @@ int main()
     Storage finished_storage;
 
     Factory factory;
-    auto    res = factory.plan(raw_storage, finished_storage, metals);
+    auto    res = factory.make(raw_storage, finished_storage, metals);
+    res.get();
 }
