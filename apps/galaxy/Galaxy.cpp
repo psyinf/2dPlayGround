6564563723ab142577df46c711d2699b73d1c5da
psy_inf <sebastian@smesserschmidt.de>
2024-10-17 18:41:56 +0200

    some system rendering started

--- apps/galaxy/Galaxy.cpp
+++ apps/galaxy/Galaxy.cpp
@@ -21,17 +21,20 @@ void galaxy::GalacticCore::setup()
     pg::game::SystemsFactory::registerSystem<galaxy::GuiRenderSystem>("guiSystem");
     pg::game::SystemsFactory::registerSystem<galaxy::TaggedRenderSystem<pg::tags::GalaxyRenderTag>>(
         "galaxyRenderSystem");
+    pg::game::SystemsFactory::registerSystem<galaxy::TaggedRenderSystem<pg::tags::SystemRenderTag>>(
+        "systemRenderSystem");
+
     // scenes
     game->createScene<galaxy::SplashScreen>("splashScreen", {.systems = {"soundSystem", "guiSystem"}});
     game->createScene<galaxy::PreLoadResources>("loadGalaxy", {.systems = {"soundSystem", "guiSystem"}});
     game->createScene<galaxy::GalaxyScene>("galaxy", {.systems = {"soundSystem", "galaxyRenderSystem", "guiSystem"}});
-    game->createScene<galaxy::SystemScene>("system", {.systems = {"soundSystem", "guiSystem"}});
+    game->createScene<galaxy::SystemScene>("system", {.systems = {"soundSystem", "systemRenderSystem", "guiSystem"}});
 
     game->addSingleton_as<pg::game::SystemInterface::Config>("guiSystem.loadGalaxy.config",
                                                              pg::game::SystemInterface::Config{{"standalone", "true"}});
-    game->addSingleton_as<pg::game::SystemInterface::Config>("guiSystem.system.config",
-                                                             pg::game::SystemInterface::Config{{"standalone", "true"}});
-    auto& scene = game->switchScene("splashScreen");
+    game->addSingleton_as<pg::game::SystemInterface::Config>(
+        "guiSystem.system.config", pg::game::SystemInterface::Config{{"standalone", "false"}});
+    auto& scene = game->switchScene("galaxy");
     scene.start();
 }
 
--- apps/galaxy/components/Tags.hpp
+++ apps/galaxy/components/Tags.hpp
@@ -6,4 +6,5 @@ using entt::literals::operator""_hs;
 using MarkerTag = entt::tag<"Marker"_hs>;
 using DebugRenderingItemTag = entt::tag<"RenderDebug"_hs>;
 using GalaxyRenderTag = entt::tag<"GalaxyRenderTag"_hs>;
+using SystemRenderTag = entt::tag<"SystemRenderTag"_hs>;
 } // namespace pg::tags
