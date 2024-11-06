e6d839672052bba84ae2fdfeffd242963a1540c1
Sebastian Messerschmidt <sebastian_messerschmidt@baxter.com>
2024-11-06 15:53:26 +0100

    WIP

---
+++ apps/galaxy/components/SoundScape.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+
+namespace galaxy {  
+struct SceneSoundScape
+{
+    //TODO: generic resource
+    std::string background_music;
+};
+
+
+struct SoundConfig
+{
+    float master_gain{1.0f};
+//     float music_gain{1.0f};
+//     float sfx_gain{1.0f};
+//     float voice_gain{1.0f};
+//     float ambiance_gain{1.0f};
+//     float ui_gain{1.0f};
+};
+
+;
+
+} // namespace galaxy
