3fee239dbef1b91cb5daf672e05f5595c5fc1562
psy_inf <sebastian@smesserschmidt.de>
2024-11-12 21:55:28 +0100

    instanced SystemsRegistry

--- libs/pgGame/systems/SystemsRegistry.hpp
+++ libs/pgGame/systems/SystemsRegistry.hpp
@@ -1,31 +1,49 @@
 #pragma once
 #include <pgf/caching/GenericFactory.hpp>
+#include <string>
 
 namespace pg::game {
 class Game;
+using namespace std::string_view_literals;
 
 class SystemsFactory
 {
     using Factory = foundation::GenericFactory<pg::game::SystemInterface, pg::game::Game&, const std::string&>;
+    using SharedSystem = std::shared_ptr<pg::game::SystemInterface>;
+    using SharedSystems = std::unordered_map<std::string, SharedSystem>;
+
+    static constexpr auto sharedSystemPrefix = "::"sv;
 
 public:
     template <typename T, typename... Args>
     static void registerSystem(std::string name)
     {
-        factory.registerPrototype(name,
-                                  foundation::GenericFactory<T, pg::game::Game&, const std::string&, Args...>::proto());
+        _factory.registerPrototype(
+            name, foundation::GenericFactory<T, pg::game::Game&, const std::string&, Args...>::proto());
     }
 
-    static auto makeSystem(std::string_view key, pg::game::Game& game)
+    static auto makeSystem(std::string_view key, pg::game::Game& game) -> std::shared_ptr<pg::game::SystemInterface>
     {
-        return factory.make(key, game, std::string{key});
+        // check if a shared system is requested
+        if (key.starts_with(sharedSystemPrefix))
+        {
+            auto sharedKey = std::string{key.substr(sharedSystemPrefix.size())};
+            if (!_sharedSystems.contains(sharedKey))
+            {
+                _sharedSystems.emplace(sharedKey, _factory.make(sharedKey, game, std::string{sharedKey}));
+            }
+            return _sharedSystems.at(sharedKey);
+        }
+
+        return _factory.make(key, game, std::string{key});
     }
 
-    static auto& getFactory() { return factory; }
+    static auto& getFactory() { return _factory; }
 
-    auto getRegisteredSystems() const { return factory.getKeys(); }
+    auto getRegisteredSystems() const { return _factory.getKeys(); }
 
 private:
-    static inline Factory factory;
+    static inline Factory       _factory;
+    static inline SharedSystems _sharedSystems;
 };
 } // namespace pg::game
