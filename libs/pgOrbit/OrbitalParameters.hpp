4bea8bf437b14d90c2e93346930c520de0ffc43d
psy_inf <sebastian@smesserschmidt.de>
2024-11-27 00:20:36 +0100

    orbital mechanics library

---
+++ libs/pgOrbit/OrbitalParameters.hpp
@@ -0,0 +1,20 @@
+#pragma once
+#include <pgOrbit/FloatingPointType.hpp>
+
+namespace pgOrbit {
+
+template <FloatingPoint T>
+struct OrbitalParameters
+{
+    // orbital parameters
+    T eccentricity;
+    T semimajor_axis;
+    T incliniation;
+    T longAN; ///> longitude of ascending node
+    T longPA; ///> longitude of periapsis, basically the orientation of the ellipse
+    /// time specific parameters
+    T meanLongitude{}; /** Mean longitude is the ecliptic longitude at which an orbiting body could be found if its
+                               orbit were circular and free of perturbations. While nominally a simple longitude, in
+                               practice the mean longitude does not correspond to any one physical angle.**/
+};
+} // namespace pgOrbit
