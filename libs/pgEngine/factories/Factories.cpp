8988ef3fe305b0d73b329edefbde219c8d7e5d33
psy_inf <sebastian@smesserschmidt.de>
2024-12-18 23:51:45 +0100

    thread-safe SDL image loading

--- libs/pgEngine/factories/Factories.cpp
+++ libs/pgEngine/factories/Factories.cpp
@@ -7,9 +7,23 @@
 
 #include <spdlog/spdlog.h>
 
+// TODO: progress callback
+static auto loadFromFile(const std::string& path)
+{
+    static std::mutex m;
+    // get the file into a std::vector<char>
+    std::ifstream     file(path, std::ios::binary);
+    std::vector<char> buff((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+    std::lock_guard<std::mutex> lock(m);
+    SDL_RWops*                  rw = SDL_RWFromMem(buff.data(), buff.size());
+    auto                        img = IMG_Load_RW(rw, 1);
+    return img;
+}
+
 sdl::Texture pg::SpriteFactory::makeTexture(sdl::Renderer& renderer, std::string_view resource_name)
 {
-    auto img = IMG_Load(resource_name.data());
+    auto img = loadFromFile(resource_name.data());
     if (!img)
     {
         spdlog::error("Failed to load image: {} {}", resource_name, IMG_GetError());
@@ -23,13 +37,14 @@ sdl::Texture pg::SpriteFactory::makeTexture(sdl::Renderer& renderer, std::string
 pg::Sprite pg::SpriteFactory::makeSprite(sdl::Renderer& renderer, std::string_view resource_name)
 {
     const auto path = _resourceLocator.locate(std::string{resource_name});
-    auto       img = IMG_Load(path.string().c_str());
+    auto       img = loadFromFile(path.string());
     if (!img)
     {
         spdlog::error("Failed to load image: {} {}", resource_name, IMG_GetError());
         sdl::Surface default_surface(SDL_CreateRGBSurface(0, 1, 1, 8, 0, 0, 0, 0));
         return Sprite(std::make_shared<sdl::Texture>(renderer.get(), default_surface.get()));
     }
+    // TODO: use IMG_loadTexture
     sdl::Surface spriteSurface(img);
     return Sprite(std::make_shared<sdl::Texture>(renderer.get(), spriteSurface.get()));
 }
