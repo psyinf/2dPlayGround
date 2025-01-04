15b2bc7c5e17f49c45de941431f4fb6430a352b8
psy_inf <sebastian@smesserschmidt.de>
2025-01-04 09:51:08 +0100

    options overhaul

--- apps/galaxy/gui/menus/OptionsMenu.hpp
+++ apps/galaxy/gui/menus/OptionsMenu.hpp
@@ -7,6 +7,30 @@
 
 namespace galaxy::gui {
 
+static constexpr auto small_galaxy_stars = 1000;
+static constexpr auto medium_galaxy_stars = 5000;
+static constexpr auto large_galaxy_stars = 15000;
+static constexpr auto huge_galaxy_stars = 25000;
+static constexpr auto massive_galaxy_stars = 50000;
+constexpr std::array  myStrings = {"one", "two", "three"};
+// array of names
+static constexpr auto galaxy_star_sizes = std::to_array({"Small", "Medium", "Large", "Huge", "Massive"});
+static constexpr auto galaxy_star_sizes_counts = std::to_array({1000u, 5000u, 10000u, 20000u, 50000u});
+
+static std::string_view getNullSeparatedNames()
+{
+    static std::string result;
+    if (result.empty())
+    {
+        for (auto& name : galaxy_star_sizes)
+        {
+            result += name;
+            result += '\0';
+        }
+    }
+    return result;
+}
+
 static bool optionsMenu(galaxy::config::Galaxy&                 galaxy_config,
                         bool&                                   closed,
                         std::function<void(const std::string&)> buttonPressed)
@@ -15,28 +39,17 @@ static bool optionsMenu(galaxy::config::Galaxy&                 galaxy_config,
     closed = false;
     using pg::gui::ButtonSize;
     ImGui::SetCursorPos(ImVec2(50, 50));
-    // menuButton(options_anchor, "Close ", [this]() { active_menu = {}; });
     ImGui::BeginChild("Options");
 
     // slider for opacity
     ImGui::SliderFloat("Background opacity", &galaxy_config.background.opacity, 0.0f, 1.0f);
     // number of stars as small (1000), medium (5000), large (15000)
     auto current_item = galaxy_config.creation.num_stars <= 1000 ? 0 : galaxy_config.creation.num_stars <= 5000 ? 1 : 2;
-
-    if (ImGui::Combo("Number of stars", &current_item, "Small\0Medium\0Large\0\0"))
+    // string from names
+    // const char* data = galaxy_star_sizes.data();
+    if (ImGui::Combo("Number of stars", &current_item, getNullSeparatedNames().data()))
     {
-        switch (current_item)
-        {
-        case 0:
-            galaxy_config.creation.num_stars = 1000;
-            break;
-        case 1:
-            galaxy_config.creation.num_stars = 5000;
-            break;
-        case 2:
-            galaxy_config.creation.num_stars = 15000;
-            break;
-        }
+        galaxy_config.creation.num_stars = galaxy_star_sizes_counts.at(current_item);
     }
     // star size
 
