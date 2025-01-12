4b064bd3d384854ce442d814746e72053de9bd81
psy_inf <sebastian@smesserschmidt.de>
2025-01-12 23:52:55 +0100

    moved ResourcePreLoader out of LoadResourcesScene impl

--- apps/galaxy/scenes/LoadResourcesScene.hpp
+++ apps/galaxy/scenes/LoadResourcesScene.hpp
@@ -2,19 +2,16 @@
 #include <pgGame/core/Scene.hpp>
 #include <memory>
 
-#include <atomic>
-#include <barrier>
 #include <pgEngine/core/FrameStamp.hpp>
 
 #include <pgGame/components/singletons/RegisteredPreloaders.hpp>
+#include <pgGame/core/ResourcePreLoader.hpp>
 #include <gui/LoadResourcesWidget.hpp>
 
 namespace galaxy {
 // TODO: pre loader per scene-transition
 class LoadResourcesScene : public pg::game::Scene
 {
-    static constexpr bool coarse = false;
-
 public:
     LoadResourcesScene(pg::game::Game& game, pg::game::SceneConfig&& scene_cfg, std::string sceneIdToPrepareFor)
       : pg::game::Scene(game, std::move(scene_cfg))
@@ -27,13 +24,6 @@ public:
     virtual ~LoadResourcesScene()
     {
         // wait for threads to finish
-
-        if (_threads_running_barrier)
-        {
-            spdlog::info("Waiting for loading threads to finish");
-            _threads_running_barrier->arrive_and_wait();
-            spdlog::info("Loading threads finished");
-        }
     }
 
     void start() override
@@ -42,18 +32,12 @@ public:
         // add percentage of resources loaded as singleton
         addSingleton_as<float&>("resourceLoader.totalProgress", _percentTotalResourcesLoaded);
         addSingleton_as<std::map<std::string, float>&>("resourceLoader.resourcesProgress", _percentResourcesLoaded);
+
         // get all registered loaders
         // TODO: this might be per scene
         auto& loaders = getGame().getSingleton<pg::singleton::RegisteredLoaders>(_sceneIdToPrepareFor + ".loaders");
-        _loaders = loaders.loaders;
-        // build thread threads and barrier
-        _threads_running_barrier = std::make_unique<std::barrier<>>(_loaders.size() + 2);
-        for (auto& [file, load_function] : _loaders)
-        {
-            createLoaderThread(std::move(load_function), file);
-        }
-        // start a watcher thread to update the progress
-        createWatchProgressThread(_loaders.size());
+        _resourcePreloader.initialize(
+            loaders.loaders, _percentResourcesLoaded, [this](float p) { _percentTotalResourcesLoaded = p; });
         setupOverlay();
         // load resources for coming scene(s)
         Scene::start();
@@ -65,70 +49,6 @@ public:
         if (totalProgress >= 1.0f)
         {
             getGame().getGlobalDispatcher().trigger<pg::game::events::SwitchSceneEvent>({_sceneIdToPrepareFor});
-        }
-    }
-
-    void createLoaderThread(Loader&& loader, const std::string& resource)
-    {
-        std::jthread([this, loader = std::move(loader), resource] {
-            try
-            {
-                spdlog::info("Pre-Loading resource: {}", resource);
-                loader(_percentResourcesLoaded);
-                std::this_thread::sleep_for(std::chrono::milliseconds(100));
-                spdlog::info("Pre-Loaded resource: {}. {} remaining", resource, _loaders.size() - _numThreadsFinished);
-            }
-            catch (const std::exception& e)
-            {
-                spdlog::error("Failed to load resource: {} ({})", resource, e.what());
-            }
-            _percentResourcesLoaded[resource] =
-                1.0f; // might have been in cache already, thus not triggering a progress callback
-            _numRead++;
-            std::unique_lock<std::mutex> lk(_mutex);
-            _cv.notify_one();
-            std::ignore = _threads_running_barrier->arrive();
-            _numThreadsFinished++;
-        }).detach();
-    }
-
-    void createWatchProgressThread(std::vector<std::string>::size_type size)
-    {
-        if (coarse)
-        {
-            std::jthread([&, this, size] {
-                while (_numRead < size)
-                {
-                    std::unique_lock<std::mutex> lk(_mutex);
-                    _cv.wait(lk);
-
-                    _percentTotalResourcesLoaded = static_cast<float>(_numRead) / size;
-                    std::this_thread::sleep_for(std::chrono::milliseconds(10));
-                }
-                std::ignore = _threads_running_barrier->arrive();
-                _numThreadsFinished++;
-            }).detach();
-        }
-        else
-        {
-            std::jthread([&, this, size] {
-                while (_numRead < size)
-                {
-                    using namespace std::chrono_literals;
-                    std::unique_lock<std::mutex> lk(_mutex);
-                    _cv.wait_for(lk, 10ms);
-
-                    auto percentTotalResourcesLoaded = 0.0f;
-                    for (const auto& [key, value] : _percentResourcesLoaded)
-                    {
-                        percentTotalResourcesLoaded += (value);
-                    }
-                    _percentTotalResourcesLoaded = percentTotalResourcesLoaded / _percentResourcesLoaded.size();
-                }
-                _percentTotalResourcesLoaded = 1.0f;
-                std::ignore = _threads_running_barrier->arrive();
-                _numThreadsFinished++;
-            }).detach();
         }
     }
 
@@ -148,16 +68,8 @@ public:
 private:
     float _percentTotalResourcesLoaded{};
 
-    PercentCompleted _percentResourcesLoaded{};
-
-    std::atomic<uint32_t> _numRead{};
-    Loaders               _loaders;
-    // mutex and condition variable to signal when a resource is loaded
-
-    std::mutex                      _mutex;
-    std::condition_variable         _cv;
-    std::unique_ptr<std::barrier<>> _threads_running_barrier;
-    std::atomic_uint32_t            _numThreadsFinished{};
-    std::string                     _sceneIdToPrepareFor;
+    PercentCompletedMap _percentResourcesLoaded{};
+    ResourcePreloader   _resourcePreloader;
+    std::string         _sceneIdToPrepareFor;
 };
 } // namespace galaxy
