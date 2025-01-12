0c75559eca2433d7aa780f22443da5599d3d38df
psy_inf <sebastian@smesserschmidt.de>
2025-01-12 23:33:45 +0100

    adapted VFSDataProvider to new interface

--- libs/pgGame/core/VFSDataProvider.cpp
+++ libs/pgGame/core/VFSDataProvider.cpp
@@ -4,19 +4,19 @@ class MemBuf : public std::basic_streambuf<char>
 {
 public:
     MemBuf(uint8_t* p, size_t l)
-        : begin(reinterpret_cast<char*>(p))
-        , end(reinterpret_cast<char*>(p + l))
+      : begin(reinterpret_cast<char*>(p))
+      , end(reinterpret_cast<char*>(p + l))
 
     {
         setg(begin, begin, end);
     }
 
-    virtual pos_type seekoff(off_type                off,
-        std::ios_base::seekdir  dir,
-        std::ios_base::openmode which = std::ios_base::in) override
+    virtual pos_type seekoff(off_type                                 off,
+                             std::ios_base::seekdir                   dir,
+                             [[maybe_unused]] std::ios_base::openmode which = std::ios_base::in) override
     {
         if (dir == std::ios_base::cur)
-            gbump(off);
+            gbump(static_cast<int>(off));
         else if (dir == std::ios_base::end)
             setg(begin, end + off, end);
         else if (dir == std::ios_base::beg)
@@ -30,28 +30,26 @@ public:
         return seekoff(pos - pos_type(off_type(0)), std::ios_base::beg, mode);
     }
 
-    char* begin, * end;
+    char *begin, *end;
 };
 
 class OwningMemBuf : public MemBuf
 {
 public:
     OwningMemBuf(std::vector<uint8_t>&& buffer)
-        : MemBuf(buffer.data(), buffer.size())
-        , _buffer(std::move(buffer)) {
-    };
+      : MemBuf(buffer.data(), buffer.size())
+      , _buffer(std::move(buffer)) {};
 
 private:
     std::vector<uint8_t> _buffer;
 };
 
-
 class MemStream : public std::istream
 {
 public:
     MemStream(uint8_t* p, size_t l)
-        : std::istream(&_buffer)
-        , _buffer(p, l)
+      : std::istream(&_buffer)
+      , _buffer(p, l)
     {
         rdbuf(&_buffer);
     }
@@ -64,8 +62,8 @@ class OwningMemStream : public std::istream
 {
 public:
     OwningMemStream(std::vector<uint8_t>&& buffer)
-        : std::istream(&_buffer)
-        , _buffer(std::move(buffer))
+      : std::istream(&_buffer)
+      , _buffer(std::move(buffer))
     {
         rdbuf(&_buffer);
     }
@@ -73,7 +71,6 @@ public:
 private:
     OwningMemBuf _buffer;
 };
-
 
 pg::VFSDataProvider::~VFSDataProvider() {}
 
@@ -83,7 +80,7 @@ void pg::VFSDataProvider::close() {}
 
 std::istream& pg::VFSDataProvider::asStream()
 {
-    auto file = _vfs->OpenFile({ getUri() }, { vfspp::IFile::FileMode::Read });
+    auto file = _vfs->OpenFile({getUri()}, {vfspp::IFile::FileMode::Read});
     if (!file) { throw std::runtime_error("File not found: " + getUri().uri); }
     auto buffer = std::vector<uint8_t>(file->Size());
     file->Read(buffer, buffer.size());
@@ -92,9 +89,17 @@ std::istream& pg::VFSDataProvider::asStream()
     return *_stream;
 }
 
-void pg::VFSDataProvider::asBuffer(std::vector<char>& buffer, size_t max_ch)
+std::vector<char> pg::VFSDataProvider::asBuffer()
 {
-    auto file = _vfs->OpenFile({ getUri() }, { vfspp::IFile::FileMode::Read });
+    auto file = _vfs->OpenFile({getUri()}, {vfspp::IFile::FileMode::Read});
+    auto buffer = std::vector<uint8_t>(file->Size());
+    file->Read(buffer, buffer.size());
+    return std::vector<char>(buffer.begin(), buffer.end());
+}
+
+void pg::VFSDataProvider::toBuffer(std::vector<char>& buffer, size_t max_ch)
+{
+    auto file = _vfs->OpenFile({getUri()}, {vfspp::IFile::FileMode::Read});
     if (max_ch == 0) { max_ch = file->Size(); }
     auto buffer_ = std::vector<uint8_t>(std::min(file->Size(), max_ch));
 
@@ -106,4 +111,18 @@ void pg::VFSDataProvider::asBuffer(std::vector<char>& buffer, size_t max_ch)
 char pg::VFSDataProvider::readNext()
 {
     return 0;
+}
+
+std::span<char> pg::VFSDataProvider::asSpan()
+{
+    thread_local auto buffer = std::vector<uint8_t>();
+    buffer.resize(size());
+
+    _file->Read(buffer, buffer.size());
+    return std::span<char>(reinterpret_cast<char*>(buffer.data()), buffer.size());
+}
+
+size_t pg::VFSDataProvider::size()
+{
+    return _file->Size();
 }
--- libs/pgGame/core/VFSDataProvider.hpp
+++ libs/pgGame/core/VFSDataProvider.hpp
@@ -7,19 +7,24 @@ class VFSDataProvider : public foundation::DataProvider
 {
     std::shared_ptr<vfspp::VirtualFileSystem> _vfs;
     std::shared_ptr<std::istream>             _stream;
+    vfspp::IFilePtr                           _file;
 
 public:
     VFSDataProvider(const foundation::URI& uri, std::shared_ptr<vfspp::VirtualFileSystem> vfs)
       : DataProvider(uri)
       , _vfs(std::move(vfs))
+      , _file(_vfs->OpenFile({getUri()}, {vfspp::IFile::FileMode::Read}))
     {
     }
 
     ~VFSDataProvider() override;
-    void          open() override;
-    void          close() override;
-    std::istream& asStream() override;
-    void          asBuffer(std::vector<char>& buffer, size_t max_ch) override;
-    char          readNext() override;
+    void              open() override;
+    void              close() override;
+    std::istream&     asStream() override;
+    std::vector<char> asBuffer() override;
+    void              toBuffer(std::vector<char>& buffer, size_t max_ch) override;
+    char              readNext() override;
+    std::span<char>   asSpan() override;
+    size_t            size() override;
 };
 } // namespace pg
