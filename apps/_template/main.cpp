c01b0870db92983d15c8e5badaee06816d3fa542
psy_inf <sebastian@smesserschmidt.de>
2025-01-28 22:46:49 +0100

    refactored _template to use minimal game/scene mechanics

--- apps/_template/main.cpp
+++ apps/_template/main.cpp
@@ -1,29 +1,60 @@
 
 #include <pgEngine/factories/Factories.hpp>
 #include <pgEngine/core/App.hpp>
-#include <pgEngine/core/ErrorTrace.hpp>
 #include <pgEngine/primitives/Renderable.hpp>
 #include <pgEngine/core/State.hpp>
+#include <pgGame/core/Game.hpp>
+
+class RenderSystem : public pg::game::SystemInterface
+{
+    struct PositionedRenderable
+    {
+        pg::Transform2D                 trans;
+        std::shared_ptr<pg::Renderable> renderable;
+    };
+
+    using Renderables = std::vector<PositionedRenderable>;
+    Renderables _renderables;
+
+public:
+    using SystemInterface::SystemInterface;
+
+    void setup(std::string_view /*scene_id*/) override
+    {
+        auto font = pg::SDLFont("../data/fonts/Roboto-Regular.ttf", 24);
+        _renderables.push_back({{.pos{100, 100}},
+                                std::make_shared<pg::Sprite>(pg::SpriteFactory::makeTextSprite(
+                                    _game.getApp().getRenderer(), font, "Hello, World!"))});
+        _renderables.push_back({{.pos{100, 400}}, std::make_shared<pg::Line>(pg::fVec2{0, 0}, pg::fVec2{100, 100})});
+    }
+
+    void handle(const pg::FrameStamp& frameStamp) override
+    {
+        auto renderer = pg::Renderer{_game.getApp().getRenderer(), frameStamp};
+        renderer.clear();
+        // color state
+        pg::ColorState colorState({0xff, 0x00, 0xff, 0xff});
+        colorState.apply(renderer.renderer);
+
+        for (auto& renderable : _renderables)
+        {
+            renderable.renderable->draw(renderer, renderable.trans, {});
+        }
+        colorState.restore(renderer.renderer);
+        renderer.present();
+    }
+};
 
 void textdemo_main()
 {
-    pg::SDLApp app{pg::config::WindowConfig{.screen{}, .offset{200, 200}, .size{1024, 1024}}};
+    pg::game::Game game({});
 
-    auto font = pg::SDLFont("../data/fonts/Roboto-Regular.ttf", 24);
-    // make a text sprite
-    auto text = pg::SpriteFactory::makeTextSprite(app.getRenderer(), font, "Hello, World!");
-    auto line = pg::Line{{0, 0}, {100, 100}};
-    auto renderer = pg::Renderer{app.getRenderer(), {}};
-    renderer.renderer.setDrawColor(
-        static_cast<uint8_t>(0xff), static_cast<uint8_t>(0x00), static_cast<uint8_t>(0xff), static_cast<uint8_t>(0xff));
+    pg::game::SystemsFactory::registerSystem<RenderSystem>("renderSystem");
 
-    auto render = [&](auto& app) {
-        text.draw(renderer, {.pos{100, 100}}, {});
-        line.draw(renderer, {.pos{100, 400}}, {});
-    };
+    game.createScene({.scene_id = "start", .systems = {"renderSystem"}});
+    game.switchScene("start");
 
-    auto done = false;
-    app.loop(done, render);
+    game.loop();
 }
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
