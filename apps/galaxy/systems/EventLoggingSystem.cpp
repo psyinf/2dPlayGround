724c6f6f61f64dcbbfa1fce58c757644f8776709
psy_inf <sebastian@smesserschmidt.de>
2025-02-05 22:28:39 +0100

    more logging and extended SystemOwnerChangedEvent

--- apps/galaxy/systems/EventLoggingSystem.cpp
+++ apps/galaxy/systems/EventLoggingSystem.cpp
@@ -1,5 +1,6 @@
 #include "EventLoggingSystem.hpp"
 #include <events/DroneEvents.hpp>
+#include <events/SystemEvents.hpp>
 #include <pgGame/core/Game.hpp>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -28,11 +29,27 @@ public:
             "Drone destroyed: {} ({})", entt::to_integral(drone_destroyed_event.entity), drone_destroyed_event.reason);
     }
 
-    static inline std::shared_ptr<spdlog::logger> logger{spdlog::stdout_color_mt("GalaxyEventLogger")};
+    static void logSystemOwnerChanged(const galaxy::events::SystemOwnerChangedEvent& system_owner_changed)
+    {
+        logger->info("System owner changed for system {}({}): {} -> {}, State from {} to {}",
+                     systemNameFromEntity(system_owner_changed.system_entity),
+                     entt::to_integral(system_owner_changed.system_entity),
+                     system_owner_changed.previous_owner,
+                     system_owner_changed.owner_faction,
+                     magic_enum::enum_name(system_owner_changed.old_status),
+                     magic_enum::enum_name(system_owner_changed.status));
+    }
+
+    static inline std::shared_ptr<spdlog::logger>          logger{spdlog::stdout_color_mt("GalaxyEventLogger")};
+    static inline std::function<std::string(entt::entity)> systemNameFromEntity{};
 };
 
 void galaxy::EventLoggingSystem::setup(std::string_view /*scene_id*/)
 {
+    EventLoggerMonostate::systemNameFromEntity = [this](entt::entity entity) {
+        auto& starsystem = _game.getGlobalRegistry().get<galaxy::StarSystemState>(entity);
+        return starsystem.name;
+    };
     _game.getDispatcher()
         .sink<galaxy::events::FactionActivatedEvent>()
         .connect<&EventLoggerMonostate::logFactionActivated>();
@@ -44,6 +61,10 @@ void galaxy::EventLoggingSystem::setup(std::string_view /*scene_id*/)
     _game.getDispatcher()
         .sink<galaxy::events::DroneDestroyedEvent>()
         .connect<&EventLoggerMonostate::logDroneDestroyed>();
+
+    _game.getDispatcher()
+        .sink<galaxy::events::SystemOwnerChangedEvent>()
+        .connect<&EventLoggerMonostate::logSystemOwnerChanged>();
 }
 
 void galaxy::EventLoggingSystem::handle(const pg::FrameStamp& /*frameStamp*/) {}
