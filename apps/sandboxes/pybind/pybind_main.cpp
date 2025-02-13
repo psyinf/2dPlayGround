4d15254cb581a86551c299422683d082849b6ac4
psy_inf <sebastian@smesserschmidt.de>
2025-02-14 00:18:23 +0100

    install dependency for python and testing error handling

--- apps/sandboxes/pybind/pybind_main.cpp
+++ apps/sandboxes/pybind/pybind_main.cpp
@@ -1,5 +1,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/embed.h> // for embedding Python interpreter
+#include <iostream>
 
 namespace py = pybind11;
 
@@ -21,17 +22,25 @@ int main()
     py::scoped_interpreter guard{};
 
     // Create the module dynamically (this is an embedded module)
-    py::module mymodule = py::module::import("mymodule");
 
     // Define some Python code to execute
-    const char* code = R"(
-        import mymodule
-        result = mymodule.add(3, 4)
-        print("Result from embedded module:", result)
-        )";
-
+    const char* code = R"(import mymodule
+result = mymodule.add(3, 4,7)
+print("Result from embedded module:", result)
+)";
+    try
+    {
+        py::module mymodule = py::module::import("mymodule");
+        // Execute the Python code using py::exec
+        // py::eval_file("script.py");
+        py::exec(code);
+    }
+    catch (const py::error_already_set& e)
+    {
+        // py::module::import("traceback").attr("print_exception")(e.type(), e.value(), e.trace());
+        std::cout << e.what() << "\n";
+    }
     // Execute the Python code using py::exec
-    py::exec(code);
 
     return 0;
 }
